74115e997f7150162405098e30f16596f5cd8e90
ForkEyeee <shawncarter123456@gmail.com>
2023-02-25 20:34:02 -0500

    added functionality for alternating markers which update gameBoard array based on position, began working on win condition function for use at the end of each turn

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 const gameBoardNodeList = document.querySelectorAll('.grid-cell');
 const gameBoardHTMLCollection = Array.from(gameBoardNodeList);
-
+let element;
+let count = 'O';
 const gameBoard = {
-  gameBoard: ['X', 'O', 'X', 'O', 'X', 'O'],
+  gameBoard: ['', '', '', '', '', '', '', '', ''],
 };
 
 const gamePlayers = {
@@ -10,14 +11,70 @@ const gamePlayers = {
   Player2: 'Jeff',
 };
 
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // vertical
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // diagonal
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+//
+
+(function () {
+  for (let i = 0; i < gameBoardHTMLCollection.length; i++) {
+    gameBoardHTMLCollection[i].addEventListener('click', (event) => {
+      if (gameBoardHTMLCollection[i].innerHTML === '') {
+        if (gameBoard.gameBoard[i] === '' && count === 'O') {
+          gameBoard.gameBoard[i] = 'X';
+        } else {
+          gameBoard.gameBoard[i] = 'O';
+        }
+
+        if (gameBoard.gameBoard.length > 9) {
+          // gameBoard.gameBoard.length = 0;
+        } else {
+          gameBoardHTMLCollection[i].innerHTML = gameBoard.gameBoard[i];
+          count = event.target.innerHTML;
+        }
+      }
+
+      //if 3 of a type of marker exists on the board, check if they are in
+
+      // element = event.target;
+      // if (gameBoardHTMLCollection[i].innerHTML === '') {
+      //   gameBoardHTMLCollection[i].innerHTML = 'X';
+      // } else if (element.innerHTML === "X"){
+      //   gameBoardHTMLCollection[i].innerHTML = 'O';
+      // }
+      console.log(element);
+      if (JSON.stringify(winConditions).includes( JSON.stringify(getInd(gameBoard.gameBoard, 'X')))) {
+        alert('You Win')
+      }
+    });
+  
+  }
+})();
+
+function getInd(arr, val) {
+  let index = [],
+    i = -1;
+  while ((i = arr.indexOf(val, i + 1)) != -1) {
+    index.push(i);
+  }
+  if (arr.length > 3) {
+    //arr.pop();
+  }
+  return index;
+}
+
 const controlGame = {
-  renderContent: () => {
-    for (let i = 0; i < gameBoardHTMLCollection.length; i++) {
-      gameBoardHTMLCollection[i].addEventListener('click', (event) => {
-        const element = event.target;
-        gameBoardHTMLCollection[i].innerHTML = 'X';
-        console.log(element);
-      });
-    }
-  },
+  renderContent: () => {},
 };
+
+//When the user presses on a grid cell, the markers will alternate between X and O.
